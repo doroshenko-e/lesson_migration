366bc91a8fa7c283cb09edc2435ee54cb88c6e61
Eugene-D <bekinassoff@gmail.com>
2016-06-08 16:22:04 +0300

    Method for active flag changed according to comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
-# 2) Добавить валидации: email уникальный, регистрация только с 15 лет, проверка формата email
+# +2) Добавить валидации: email уникальный, регистрация только с 15 лет, проверка формата email
 # 3) Создать scope который будет выводить активных пользователей старше 21 года
 # 4) Написать self метод search который будет искать пользователей по имени и фамилии
-# 5) Добавить callback который перед save будет смотреть если Имя или Фамилия не указаны то active флаг становится в false. Если всё указано, то тогда true
-
-
+# +5) Добавить callback который перед save будет смотреть если Имя или Фамилия не указаны то active флаг становится в false. Если всё указано, то тогда true
 
 class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :email, :birthday, :active
   #scope :name, -> {where ("first_name ILIKE 'j%'")}
   #scope :age, ->{where ("Date.today - 21 >= ?", birthday)}
+  scope :age_active, -> {where ()}
+  
   validates :email, :uniqueness => {:case_sensative => false}
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i { message: "That's not an email address" }
 
@@ -18,17 +18,20 @@ class User < ActiveRecord::Base
   #def costom_validation_name
   #	
   #end
+
+  before_save do |user|
+  	user.active = true if user.first_name.present? && user.last_name.present?
+  	else
+  		user.active = false
+  	end
+  end
+
+  private
+
   def age_fifteen
   	if ((Date.today - birthday) / 365).floor < 15
   		errors.add(:birthday, "can't be bellow 15")
   	end
   end
 
-  before_save do |user|
-  	user.active = false if user.first_name.blank? || user.last_name.blank?
-  	else
-  		user.active = true
-  	end
-  end
-  
 end
