77869a464c82fc047b7d1439f651d863cf56d12a
Eugene-D <bekinassoff@gmail.com>
2016-06-11 14:44:07 +0300

    migration of image table, success

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160607063810) do
+ActiveRecord::Schema.define(:version => 20160611092807) do
+
+  create_table "images", :force => true do |t|
+    t.string   "image_type"
+    t.integer  "imageable_id"
+    t.string   "imageable_type"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string  "first_name", :null => false
