8b68d5ba888e46f2d3d0b510c92fd6df0d72dd1a
Eugene-D <bekinassoff@gmail.com>
2016-06-14 12:41:11 +0300

    Scope for active users under 21 and validation method for users below 15 corrected

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,38 +1,38 @@
 # +2) Добавить валидации: email уникальный, регистрация только с 15 лет, проверка формата email
-# +/- 3) Создать scope который будет выводить активных пользователей старше 21 года
-# 4) Написать self метод search который будет искать пользователей по имени и фамилии
+# +3) Создать scope который будет выводить активных пользователей старше 21 года
+# +4) Написать self метод search который будет искать пользователей по имени и фамилии
 # +5) Добавить callback который перед save будет смотреть если Имя или Фамилия не указаны то active флаг становится в false. Если всё указано, то тогда true
 
 class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :email, :birthday, :active
   has_many :images, as: :imageable
 
-  #scope :age, where("Date.today - 21.year >= ?", :birthday)
-  scope :active, where(:active => true)
-  #scope :active_above, -> {active, where("Date.today - 21.year >= ?", :birthday)} 
+  scope :age, where("birthday < ?", 21.year.ago) #under 21 year
+  scope :active, where(:active => true) #active
+  scope :active_above, -> { active.age } #active under 21 year
   
-  #validate :age_fifteen?
+  validate :age_fifteen?, :massage => "can't be bellow 15"
   validates :email, :uniqueness => {:case_sensative => false}
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 
   before_save do |user|
   	user.active = true if user.first_name.present? && user.last_name.present?
-  	#else
-  	#user.active = false
-  	#end
   end
 
   private
-  #something wrong
+
+  #Method for age validation
   def age_fifteen?
-  	if (Date.today - 15.year) >= birthday
+  	if (self.birthday + 15.year) < Date.today
   		errors.add(:birthday, "can't be bellow 15")
   	end
   end
 
-  #selects users with query ni first nae or last name
+  #selects users with query in first or last name 
   def self.search_name(query)
-  	where("first_name ILIKE ? or last_name ILIKE ?", "%#{query}%", "%#{query}%")# | ("last_nmae ILIKE ?", "%#{query}%")} 
+  	#don't know which line better
+  	where("first_name ILIKE :query or last_name ILIKE :query", :query => "%#{query}%")
+  	#where("first_name ILIKE ? or last_name ILIKE ?",  "%#{query}%", "%#{query}%")
   end
 
 end
