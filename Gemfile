a42985bc573610aa98028525e6fc8c27b825baad
Eugene-D <bekinassoff@gmail.com>
2016-06-17 14:39:17 +0300

    User create via ajax

--- Gemfile
+++ Gemfile
@@ -22,6 +22,9 @@ end
 
 gem 'jquery-rails'
 
+#web server
+gem 'thin'
+
 # To use ActiveModel has_secure_password
 gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   
-  #def index
-  #end
+  def index
+  
+  end
 
   def new
   	@user = User.new
@@ -9,15 +10,25 @@ class UsersController < ApplicationController
 
   def create
   	@user = User.new(params[:user])
-  	if @user.save
-  		redirect_to root_url, notice: "Thnx for signing up"
-  	else
-  		render "new"
-  	end
+     respond_to do |format|
+      if @user.save
+        format.html { redirect_to users_path, notice: 'User was successfully created.' }
+        format.js
+      else
+        format.html { render action: "new" }
+      end
+    end
   end
 
-  #def update
-  #end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(params[:user])
+    respond_to do |format|
+      format.html { redirect_to users_path }
+      format.js
+    end  
+  end
 
   #def delete
   #end
