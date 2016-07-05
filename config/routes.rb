4412767eaeffc59c09a0b5d47a92f340d3c5fa05
Eugene-D <bekinassoff@gmail.com>
2016-07-05 15:25:07 +0300

    Jquery sortable works (without saving positions)

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Lesson2::Application.routes.draw do
 
   resources :sessions
   resources :users
-  map.resources :users, :has_many => :images
   root to: 'users#index'
 
   # The priority is based upon order of creation:
